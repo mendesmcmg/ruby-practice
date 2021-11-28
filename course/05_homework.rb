0e5b046ba26b68e3b24d245266483da98e113999
Clara Mendes <claraplanejamento@gmail.com>
2021-11-28 14:08:24 -0300

    :sparkles: adds homework 5

---
+++ course/05_homework.rb
@@ -0,0 +1,36 @@
+dial_book = {
+  "newyork" => "212",
+  "newbrunswick" => "732",
+  "edison" => "908",
+  "plainsboro" => "609",
+  "sanfrancisco" => "301",
+  "miami" => "305",
+  "paloalto" => "650",
+  "evanston" => "847",
+  "orlando" => "407",
+  "lancaster" => "717"
+}
+ 
+def get_city_names(somehash)
+  somehash.each do |city, code|
+    puts city
+  end
+end
+ 
+def get_area_code(somehash, key)
+  puts "The area code for #{key} is #{somehash[key]}"
+end
+ 
+loop do
+  puts "Do you want to lookup an area code based on a city name? (Y/N)"
+  ans = gets.chomp.downcase
+  if ans == "y"
+    puts "Which city do you want the area code for?"
+    get_city_names(dial_book)
+    puts "Enter your selection"
+    city = gets.chomp.downcase
+    get_area_code(dial_book, city)
+  else
+    break
+  end
+end
