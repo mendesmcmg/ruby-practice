da152d155c6ee7ec48b7b59e588bf55968e1ef90
Clara Mendes <claraplanejamento@gmail.com>
2021-11-28 13:20:41 -0300

    :sparkles: adds homework 1

---
+++ course/01_homework.rb
@@ -0,0 +1,9 @@
+puts "Gimme your first name"
+first = gets.chomp
+puts "Gimme your last name"
+last = gets.chomp
+full = "#{first} #{last}"
+count = first + last
+puts "Your full name is #{full}"
+puts "Your name reversed is #{full.reverse}"
+puts "Your name has #{count.length} characters in it"
---
+++ ruby/meetup/meetup.rb
@@ -0,0 +1,11 @@
+class Meetup
+def initialize(month, year)
+  @month = month
+  @year = year
+end
+
+def day(:weekday, :order)
+
+end
+
+end
