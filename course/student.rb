f44ff1a058351beae367b8bd5ce7023704f678e7
Clara Mendes <claraplanejamento@gmail.com>
2022-03-17 14:55:39 -0300

    :sparkles: adds student class attr

---
+++ course/student.rb
@@ -0,0 +1,21 @@
+class Student
+  attr_accessor :name, :last_name, :email, :username, :password
+
+	def initialize(firstname, lastname, username, email, password)
+		@name = firstname
+		@last_name = lastname
+		@username = username
+		@email = email
+		@password = password
+	end
+
+	def to_s
+		"Hi, #{@name} #{@last_name}"
+		"First name: #{@name}, Last name: #{@last_name}, Username: #{@username}, Email: #{@email}"
+	end
+end
+
+leia = Student.new("leia", "organa", "leia", "leia@email", "123")
+puts leia
+luke = Student.new("luke", "skywalker", "luke", "luke@email", "123")
+puts luke
