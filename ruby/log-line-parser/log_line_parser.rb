fadee0b935e956467211e0c96ccdd38dfe2b5fce
Clara Mendes <claraplanejamento@gmail.com>
2022-05-27 16:11:08 -0300

    :sparkles: adds log-line-parser exercise

---
+++ ruby/log-line-parser/log_line_parser.rb
@@ -0,0 +1,18 @@
+class LogLineParser
+  def initialize(line)
+    @line = line
+  end
+
+  def message
+    beginning = @line.index(':')
+    @line.slice(beginning + 1, @line.length-1).strip()
+  end
+
+  def log_level
+    @line.slice(@line.index('[')+1, @line.index(']')-1).downcase 
+  end
+
+  def reformat
+    "#{message()} (#{log_level()})"
+  end
+end
