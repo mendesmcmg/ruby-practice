6a3161d1a89b17b38ea03dd39a559ab97f86c901
Clara Mendes <claraplanejamento@gmail.com>
2022-05-28 18:00:23 -0300

    :sparkles: adds assembly line exercise

---
+++ ruby/assembly-line/assembly_line.rb
@@ -0,0 +1,25 @@
+class AssemblyLine
+  def initialize(speed)
+    @items_per_hour = 221.0
+    @speed = speed
+  end
+
+  def production_rate_per_hour
+    @speed
+    if @speed >= 1 and @speed <= 4 
+      production = (@speed * @items_per_hour)
+    elsif @speed >= 5 and @speed <= 8
+      production = (@speed * @items_per_hour)*0.9
+    elsif @speed == 9
+      production = (@speed * @items_per_hour)*0.8
+    elsif @speed == 10
+      production = (@speed * @items_per_hour)*0.77
+    end
+    production
+  end
+
+  def working_items_per_minute
+    working_items = production_rate_per_hour/60
+    working_items.to_i
+  end
+end
