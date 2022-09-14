378c91dc9ffe89059d396bd5993acb76f2380c2e
Clara Mendes <claraplanejamento@gmail.com>
2022-09-14 16:00:58 -0300

    :sparkles: adds hamming exercise

---
+++ ruby/hamming/hamming.rb
@@ -0,0 +1,11 @@
+class Hamming
+  def self.compute(strand1, strand2)
+    length1 = strand1.length
+    length2 = strand2.length
+    raise ArgumentError if length1 != length2
+
+    (0...length1).count do |index|
+      strand1[index] != strand2[index]
+    end
+  end
+end
