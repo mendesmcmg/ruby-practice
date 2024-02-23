e8b6f9154e0239ed90dc2181cf03cfb7fd48e062
mendesmcmg <claraplanejamento@gmail.com>
2024-02-23 15:28:24 -0300

    :sparkles: adds meetup exercise

--- ruby/meetup/meetup.rb
+++ ruby/meetup/meetup.rb
@@ -1,11 +1,50 @@
+# frozen_string_literal: true
+
+require 'date'
+
 class Meetup
   def initialize(month, year)
     @month = month
     @year = year
   end
 
-  def day(:weekday, :order)
-
+  def day(day, descriptor)
+    case descriptor
+    when :first
+      first_to_fourth_interval(day, 1)
+    when :second
+      first_to_fourth_interval(day, 2)
+    when :third
+      first_to_fourth_interval(day, 3)
+    when :fourth
+      first_to_fourth_interval(day, 4)
+    when :last
+      last(day)
+    when :teenth
+      teenth(day)
+    end
   end
 
+  def teenth(day)
+    (13..19).each do |i|
+      date = Date.new(@year, @month, i)
+      return date if date.send("#{day}?")
+    end
+  end
+
+  def first_to_fourth_interval(day, descriptor)
+    first = (descriptor - 1) * 7 + 1
+    last = descriptor * 7
+
+    (first..last).each do |i|
+      date = Date.new(@year, @month, i)
+      return date if date.send("#{day}?")
+    end
+  end
+
+  def last(day)
+    date = Date.new(@year, @month, -1)
+    date -= 1 until date.send("#{day}?")
+    date
+  end
 end
