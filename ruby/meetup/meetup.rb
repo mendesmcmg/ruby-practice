29e0757aae2ae8fb5f1cf235b6fd8594b4300f7c
mendesmcmg <claraplanejamento@gmail.com>
2024-03-15 12:05:46 -0300

    :recycle: refactors meetup exercise

--- ruby/meetup/meetup.rb
+++ ruby/meetup/meetup.rb
@@ -13,21 +13,25 @@ class Meetup
   def day(day, descriptor)
     case descriptor
     when :last
-      find_date_by_week(day, -7..-1)
+      find_date_by_range(day, -7..-1)
     when :teenth
-      find_date_by_week(day, 13..19)
+      find_date_by_range(day, 13..19)
     else
-      week_index = SCHEDULE.index(descriptor) + 1
-      first = (week_index - 1) * 7 + 1
-      last = week_index * 7
-      find_date_by_week(day, first..last)
+      week_index = SCHEDULE.index(descriptor)
+      find_date_by_week(day, week_index + 1)
     end
   end
 
-  def find_date_by_week(day, range)
+  def find_date_by_range(day, range)
     range.each do |i|
       date = Date.new(@year, @month, i)
       return date if date.send("#{day}?")
     end
+  end
+
+  def find_date_by_week(day, week_index)
+    first = (week_index - 1) * 7 + 1
+    last = week_index * 7
+    find_date_by_range(day, (first..last))
   end
 end
